2a49044ca46ba9ca84fc642206543ec1614785d9
Simon Stark <simon.stark54@gmail.com>
2024-01-18 16:15:06 +0100

    Added back button to go to Main Site

--- app/src/main/java/com/example/qralarm/AlarmListActivity.java
+++ app/src/main/java/com/example/qralarm/AlarmListActivity.java
@@ -1,6 +1,9 @@
 package com.example.qralarm;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AlarmListActivity extends AppCompatActivity {
@@ -9,5 +12,20 @@ public class AlarmListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_list);
+
+        ImageButton backButton = findViewById(R.id.back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateBackToMainActivity();
+            }
+        });
+    }
+
+    private void navigateBackToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
     }
 }
