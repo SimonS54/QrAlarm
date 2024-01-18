179deda166572b98b86613d485c3499bbea3234b
Simon Stark <simon.stark54@gmail.com>
2024-01-18 14:26:45 +0100

    Added basic vibration and some resizing

--- app/src/main/java/com/example/qralarm/AlarmReceiver.java
+++ app/src/main/java/com/example/qralarm/AlarmReceiver.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -15,6 +18,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         Toast.makeText(context, "Alarm! Your task to be executed here.", Toast.LENGTH_SHORT).show();
 
         playDefaultAlarmSound(context);
+        startVibration(context);
     }
 
     private void playDefaultAlarmSound(Context context) {
@@ -25,7 +29,20 @@ public class AlarmReceiver extends BroadcastReceiver {
         }
 
         Ringtone ringtone = RingtoneManager.getRingtone(context, alarmSound);
-
         ringtone.play();
+    }
+
+    private void startVibration(Context context) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+
+        if (vibrator != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                VibrationEffect vibrationEffect = VibrationEffect.createWaveform(
+                        new long[]{0, 1000, 1000}, 0);
+                vibrator.vibrate(vibrationEffect);
+            } else {
+                vibrator.vibrate(new long[]{0, 1000, 1000}, 0);
+            }
+        }
     }
 }
